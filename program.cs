b444bb7967288565afa880ee2e6dd23a4d7d30a0
vimalkumar singh <vimalsingh2910@gmail.com>
2025-08-05 10:17:47 +0530

    program

---
+++ program.cs
@@ -0,0 +1,7 @@
+ public int Add(int a, int b) => a + b;
+
+        public int Multiply(int a, int b) => a * b;
+
+        public int Subtract(int a, int b) => a - b;
+
+        public int Divide(int a, int b) => a / b;
